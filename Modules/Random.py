b86b1c2db6de4381ac9757a68773e834b273557e
Purna Sairam Yeluri <74450636+Sairam-Yeluri@users.noreply.github.com>
2021-07-26 13:44:22 +0530

    Update Random.py

--- Modules/Random.py
+++ Modules/Random.py
@@ -6,9 +6,9 @@ from random import *
 print("Random number(between 0 and 1)",random())
 #any random from 0 to 1
 
-l = [1,2,3,4,5,6]
-print("Random number from choice is",choice(l))
-# any random in l
+lst = [1,2,3,4,5,6]
+print("Random number from choice is",choice(lst))
+# any random in lst
 
 print("Randint function",randint(10,100))
 #Prints a number in specified range (as a 6 sided die)
@@ -25,5 +25,16 @@ print("Random Choice value is: ",cho)
 
 lst = ['a','b','c','d']
 code = choices(lst,k=6)
-print(code)
-#randomly choose values from list 
+print(code)    #randomly choose values from list 
+code2 = choices(lst,weights=[4,4,2],k=10)
+print(code2)    #Weights means probability
+code3 = choices(lst,weights=[4,4,1],k=10)
+print(code3)
+
+nums = list(range(1,10))
+print(nums)
+shuffle(nums)
+print(nums)
+
+deck = list(range(1,53))
+print(sample(deck,13))
